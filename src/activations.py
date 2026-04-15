6eaf1e183ab6e3d8555e2c4f1f5641529b96ae1f
vellar <velimir.mirchev@gmail.com>
2026-04-15 17:21:18 +0300

    add weight initialization in LinearLayer and create BaseLayer for reuse

--- src/activations.py
+++ src/activations.py
@@ -1,11 +1,12 @@
 import numpy as np
+from layers import BaseLayer
 
-class ReLU:
+class ReLU(BaseLayer):
+  
+  def forward(self, x):
+    self.x = x
+    return np.maximum(0, x)
 
-    def forward(self, x):
-        self.x = x
-        return np.maximum(0, x)
-
-    def backward(self, dout):
-        dx = dout * (self.x > 0.0)
-        return dx
+  def backward(self, dout):
+    dx = dout * (self.x > 0.0)
+    return dx
--- src/layers.py
+++ src/layers.py
@@ -1,20 +1,52 @@
+from abc import ABC, abstractmethod
 import numpy as np
 
-class LinearLayer:
-    def __init__(self, in_features:int, out_features:int):
-        self.w = 0.01 * np.random.randn(in_features, out_features) # make weights really small
-        self.b = np.zeros((1, out_features)) # we need one bias per output feature
+class BaseLayer(ABC):
+  is_training: bool
 
-    def forward(self, x):
-        self.x = x
-        return x @ self.w + self.b
+  def train(self):
+    self.is_training = True
 
-    def backward(self, dout):
-        self.dw = self.x.T @ dout
-        self.db = np.sum(dout, axis=0, keepdims=True)
-        dx = dout @ self.w.T
-        return dx
+  def eval(self):
+    self.is_training = False
 
-    def step(self, lr):
-        self.w = self.w - (self.dw * lr)
-        self.b = self.b - (self.db * lr)
+  @abstractmethod
+  def forward(self, x):
+    raise NotImplementedError
+
+  @abstractmethod
+  def backward(self, dout):
+    raise NotImplementedError
+
+  def step(self, lr):
+    pass
+    
+class LinearLayer(BaseLayer):
+  def __init__(self, in_features:int, out_features:int, initialization: str = "he"):
+
+    if initialization == "he":
+        scale = np.sqrt(2.0 / in_features)
+    elif initialization == "xavier":
+        scale = np.sqrt(1.0 / in_features)
+    elif initialization == "standard":
+        scale = 0.01
+    else:
+        raise ValueError(f"Unknown init: {initialization}")
+
+    self.w = np.random.randn(in_features, out_features) * scale
+
+    self.b = np.zeros((1, out_features)) # we need one bias per output feature
+
+  def forward(self, x):
+    self.x = x
+    return x @ self.w + self.b
+
+  def backward(self, dout):
+    self.dw = self.x.T @ dout
+    self.db = np.sum(dout, axis=0, keepdims=True)
+    dx = dout @ self.w.T
+    return dx
+
+  def step(self, lr):
+    self.w = self.w - (self.dw * lr)
+    self.b = self.b - (self.db * lr)
